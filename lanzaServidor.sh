fb323bdd84c52e934b4a6df78e7fba7a0ae82717
Izanosky <darkop895@gmail.com>
2024-12-11 20:29:30 +0100

    si

---
+++ lanzaServidor.sh
@@ -0,0 +1,11 @@
+./servidor
+ ./cliente TCP &
+ ./cliente TCP David &
+ ./cliente TCP @nogal.usal.es &
+ ./cliente TCP zxcvb@nogal.usal.es &
+ ./cliente TCP p1777001@nogal.usal.es &
+ ./cliente UDP &
+ ./cliente UDP david &
+ ./cliente UDP @nogal.usal.es &
+ ./cliente UDP zxcvb@nogal.usal.es &
+ ./cliente UDP p1777001@nogal.usal.es &
